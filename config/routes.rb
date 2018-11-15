0c5b19999a922261b3e33ac54edfa9cc0e624b6f
Louvivien <vivien.richaud@gmail.com>
2018-11-15 16:18:10 +0100

    ajout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 	root 'home#index'
         devise_for :users
         get '/landing1', to: 'home#landing1'
+        post '/landing1', to: 'home#landing1create'
+
         get '/landing2', to: 'home#landing2'
+        post '/landing1', to: 'home#landing1create'
+
         get '/landing3', to: 'home#landing3'
+       	post '/landing1', to: 'home#landing1create'
+
 end
