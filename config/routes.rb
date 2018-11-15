a917f3788b39693f477af651440139991dbaab86
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-15 15:58:21 +0100

    add style

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-       
-root 'home#index'
-
-          devise_for :users
-        get '/etudiants', to: 'home#landing1'
-
-
-
+	root 'home#index'
+        devise_for :users
+        get '/landing1', to: 'home#landing1'
+        get '/landing2', to: 'home#landing2'
+        get '/landing3', to: 'home#landing3'
 end
-
