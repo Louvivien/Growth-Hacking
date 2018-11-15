f14250aa9974bd4ae49ab573832eab86c18deace
Louvivien <vivien.richaud@gmail.com>
2018-11-15 16:21:16 +0100

    ajout controller home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,9 +12,56 @@ before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 
 	def landing1
-
-
+	gibbon = Gibbon::Request.new(api_key: "ff53197472e303cf758de97f03779caf-us19")
 	end
+
+
+	def landing2
+	gibbon = Gibbon::Request.new(api_key: "ff53197472e303cf758de97f03779caf-us19")
+	end
+
+
+	def landing3
+	gibbon = Gibbon::Request.new(api_key: "ff53197472e303cf758de97f03779caf-us19")
+	end
+
+	def landing1create
+gibbon = Gibbon::Request.new(api_key: "ff53197472e303cf758de97f03779caf-us19")
+	gibbon.timeout = 10
+	gibbon.lists("98a25261c4").members.create(
+		body: {
+
+			email_address: params[:email],
+			status: "subscribed",
+			FNAME: "First name",
+			LNAME: "Last name"
+				})	end
+
+
+	def landing2create
+gibbon = Gibbon::Request.new(api_key: "ff53197472e303cf758de97f03779caf-us19")
+	gibbon.timeout = 10
+	gibbon.lists("98a25261c4").members.create(
+		body: {
+
+			email_address: params[:email],
+			status: "subscribed",
+			FNAME: "First name",
+			LNAME: "Last name"
+				})	end
+
+
+	def landing3create
+gibbon = Gibbon::Request.new(api_key: "ff53197472e303cf758de97f03779caf-us19")
+	gibbon.timeout = 10
+	gibbon.lists("98a25261c4").members.create(
+		body: {
+
+			email_address: params[:email],
+			status: "subscribed",
+			FNAME: "First name",
+			LNAME: "Last name"
+				})	end
 
 
 
